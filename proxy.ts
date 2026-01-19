622fff48f553ccadaf294fe1e9412ec22a69b69b
Ak David <akdavid4real@gmail.com>
2026-01-19 23:35:36 +0100

    omo

--- proxy.ts
+++ proxy.ts
@@ -3,6 +3,8 @@ import { type NextRequest } from "next/server";
 
 export async function proxy(request: NextRequest) {
   console.log('[Proxy Checking] URL:', request.nextUrl.pathname)
+  console.log('[Proxy Config] URL:', process.env.NEXT_PUBLIC_SUPABASE_URL)
+  console.log('[Proxy Config] Key Start:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.substring(0, 10))
   return await updateSession(request);
 }
 
