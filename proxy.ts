1abf6f0f90f674e611f1b1c4a7d7f8ce9d458b3b
Ak David <akdavid4real@gmail.com>
2026-01-19 23:31:15 +0100

    chore: Add a console log to display the incoming proxy request URL.

--- proxy.ts
+++ proxy.ts
@@ -2,6 +2,7 @@ import { updateSession } from "@/lib/supabase/proxy";
 import { type NextRequest } from "next/server";
 
 export async function proxy(request: NextRequest) {
+  console.log('[Proxy Checking] URL:', request.nextUrl.pathname)
   return await updateSession(request);
 }
 
